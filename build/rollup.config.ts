c9388fad381ebe6353223d545758e51944af3bf8
leegeunhyeok <dev.ghlee@gmail.com>
2021-01-08 01:30:32 +0900

    refactor: update rollup build config
    - add terser for minify

--- build/rollup.config.ts
+++ build/rollup.config.ts
@@ -2,12 +2,28 @@ import { join } from 'path';
 import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: join(__dirname, '../src/index.ts'),
-  output: {
-    name: 'name',
-    exports: 'auto',
-  },
+  output: [
+    {
+      name: 'BoxDB',
+      file: 'dist/bxd.js',
+      format: 'umd',
+      exports: 'default',
+    },
+    {
+      name: 'BoxDB',
+      file: 'dist/bxd.min.js',
+      format: 'umd',
+      exports: 'default',
+      plugins: [terser()],
+    },
+    {
+      file: 'dist/bxd.esm.js',
+      format: 'esm',
+    },
+  ],
   plugins: [json(), commonjs(), typescript()],
 };
