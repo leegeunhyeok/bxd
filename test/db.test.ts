99a976bc8c5feee114588a18ae145969dae7fa98
leegeunhyeok <dev.ghlee@gmail.com>
2021-01-09 15:17:34 +0900

    test: add ts-jest for typescript testing

--- test/db.test.ts
+++ test/db.test.ts
@@ -1,5 +1,15 @@
 import 'fake-indexeddb/auto';
+import Box from '../src';
 
 test('Check IndexedDB is available', () => {
   expect(self.indexedDB).not.toBe(undefined);
+});
+
+test('Create new Box instance', () => {
+  const name = 'test-db';
+  const version = 1;
+  const box = new Box(name, version);
+
+  expect(box.databaseName).toBe(name);
+  expect(box.version).toBe(version);
 });
