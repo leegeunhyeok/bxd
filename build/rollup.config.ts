a0df1ba32a75ffad00fc19654bed87ec8faa79e6
leegeunhyeok <dev.ghlee@gmail.com>
2021-01-11 17:58:34 +0900

    build: update rollup build config (d.ts)

--- build/rollup.config.ts
+++ build/rollup.config.ts
@@ -3,27 +3,35 @@ import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: join(__dirname, '../src/index.ts'),
-  output: [
-    {
-      name: 'BoxDB',
-      file: 'dist/bxd.js',
-      format: 'umd',
-      exports: 'named',
-    },
-    {
-      name: 'BoxDB',
-      file: 'dist/bxd.min.js',
-      format: 'umd',
-      exports: 'named',
-      plugins: [terser()],
-    },
-    {
-      file: 'dist/bxd.esm.js',
-      format: 'esm',
-    },
-  ],
-  plugins: [json(), commonjs(), typescript()],
-};
+export default [
+  {
+    input: join(__dirname, '../src/index.ts'),
+    output: [
+      {
+        name: 'BoxDB',
+        file: 'dist/bxd.js',
+        format: 'umd',
+        exports: 'named',
+      },
+      {
+        name: 'BoxDB',
+        file: 'dist/bxd.min.js',
+        format: 'umd',
+        exports: 'named',
+        plugins: [terser()],
+      },
+      {
+        file: 'dist/bxd.esm.js',
+        format: 'esm',
+      },
+    ],
+    plugins: [json(), commonjs(), typescript()],
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'types/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
