b69986684c9eaef1217fe1c2c51120408830684a
leegeunhyeok <dev.ghlee@gmail.com>
2021-01-11 21:21:54 +0900

    feat!: add BoxModelRegister (idb versioning is important)
    - update regist model process

--- src/core/db.ts
+++ src/core/db.ts
@@ -1,11 +1,18 @@
 import { generateModel, BoxScheme, BoxModel } from './model';
 import { BoxDBError } from './errors';
 
+interface BoxModelMeta {
+  scheme: BoxScheme;
+  targetVersion: number;
+}
+
+type BoxModelRegister = <S extends BoxScheme>(storeName: string, scheme: S) => BoxModel<S>;
+
 class BoxDB {
   private _init = false;
   private _databaseName: string;
   private _version: number;
-  private _models: Map<string, BoxScheme> = new Map();
+  private _models: Map<number, Map<string, BoxModelMeta>> = new Map();
 
   /**
    * @constructor
@@ -27,18 +34,29 @@ class BoxDB {
 
   /**
    * regist data model for create object store
-   * @param storeName object store name
-   * @param scheme object store data structure
+   * @param targetVersion target idb version
    */
-  model<S extends BoxScheme>(storeName: string, scheme: S): BoxModel<S> {
-    if (this._init) {
-      throw new BoxDBError('database already open');
-    }
-    if (this._models.has(storeName)) {
-      throw new BoxDBError(`${storeName} model exist`);
-    }
-    this._models.set(storeName, scheme);
-    return generateModel(storeName, scheme);
+  model(targetVersion: number): BoxModelRegister {
+    /**
+     * regist data model for create object store
+     * @param storeName object store name
+     * @param scheme object store data structure
+     */
+    return <S extends BoxScheme>(storeName: string, scheme: S): BoxModel<S> => {
+      if (this._init) {
+        throw new BoxDBError('database already open');
+      }
+
+      // create new Map if version map is not exist
+      this._models.has(targetVersion) || this._models.set(targetVersion, new Map());
+
+      const versionMap = this._models.get(targetVersion);
+      if (versionMap.has(storeName)) {
+        throw new BoxDBError(`version ${targetVersion}, ${storeName} model exist`);
+      }
+      versionMap.set(storeName, { scheme, targetVersion });
+      return generateModel(storeName, scheme);
+    };
   }
 
   /**
--- test/db.test.ts
+++ test/db.test.ts
@@ -1,9 +1,11 @@
 import 'fake-indexeddb/auto';
 import Box, { BoxField } from '../src';
 
+// global instance variable for test
 let box: Box = null;
 
 test('Check IndexedDB is available', () => {
+  // #1. checking about idb supports
   expect(self.indexedDB).not.toBe(undefined);
 });
 
@@ -12,13 +14,14 @@ test('Create new Box instance', () => {
   const version = 1;
   box = new Box(name, version);
 
+  // #1. check basic information
   expect(box.databaseName).toBe(name);
   expect(box.version).toBe(version);
 });
 
 test('Create new Model', () => {
   // regist new model
-  const User = box.model('user', {
+  const User = box.model(1)('user', {
     name: new BoxField(String),
     age: new BoxField(Number),
   });
@@ -28,12 +31,24 @@ test('Create new Model', () => {
   user.age = 10;
   user.name = 'tom';
 
+  // #1. change test data
   expect(user.age).toBe(10);
+
+  // #2. to be thrown exception
   expect(() => {
-    // trying to regist same model name
-    box.model('user', {
+    // trying to register same target version and model name
+    box.model(1)('user', {
       name: new BoxField(String),
       age: new BoxField(Number),
     });
   }).toThrow();
+
+  // #3. to be not thrown exception
+  expect(() => {
+    // trying to register same model name (with diffrent target version)
+    box.model(2)('user', {
+      name: new BoxField(String),
+      age: new BoxField(Number),
+    });
+  }).not.toThrow();
 });
