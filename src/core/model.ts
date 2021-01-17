6ae9812cdd56a49332cbcefa1b62cf059320fda5
leegeunhyeok <dev.ghlee@gmail.com>
2021-01-17 21:24:53 +0900

    refactor: update module file name

--- src/core/model.ts
+++ src/core/model.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import BoxDB from './db';
+import BoxDB from './database';
 import { BoxDBError } from './errors';
 
 export interface BoxScheme {
