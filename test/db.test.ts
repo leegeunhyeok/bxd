7e69139d5ee6db23c9a9539b17132151e163d618
leegeunhyeok <dev.ghlee@gmail.com>
2021-01-21 16:28:55 +0900

    test: update test code

--- test/db.test.ts
+++ test/db.test.ts
@@ -6,7 +6,7 @@ let box: Box = null;
 
 test('Create new Box instance', () => {
   const name = 'test-db';
-  const version = 1;
+  const version = 5;
   box = new Box(name, version);
 
   // #1. check basic information
@@ -17,6 +17,10 @@ test('Create new Box instance', () => {
 test('Create new Model', () => {
   // regist new model
   const User = box.model(1)('user', {
+    id: {
+      type: Types.NUMBER,
+      key: true,
+    },
     name: {
       type: Types.STRING,
       index: true,
@@ -24,37 +28,99 @@ test('Create new Model', () => {
     age: Types.NUMBER,
   });
 
-  // create new data
-  const user = new User();
-  user.age = 10;
-  user.name = 'Tom';
-
-  // #1. change test data
-  expect(user.age).toBe(10);
-
-  // #2. create new data with initial value
+  // #2. create new data
   expect(() => {
-    new User({
+    const test = {
+      id: 1,
       name: 'Tom',
-      age: 1,
-    });
-  }).not.toThrow();
+      age: 10,
+    };
 
-  // #3. to be thrown exception
-  expect(() => {
-    // trying to register same target version and model name
-    box.model(1)('user', {
-      name: Types.STRING,
-      age: Types.NUMBER,
+    // Test data 1 (basic)
+    const user1 = new User();
+    user1.id = test.id;
+    user1.name = test.name;
+    user1.age = test.age;
+
+    // Test data 2 (with inital value)
+    const user2 = new User({
+      id: test.id,
+      name: test.name,
+      age: test.age,
     });
+
+    return user1.id === user2.id && user1.name === user2.name && user1.age === user2.age;
+  }).toBeTruthy();
+
+  // #3. trying to register same target version and model name
+  expect(() => {
+    try {
+      box.model(1)('user', {
+        id: {
+          type: Types.NUMBER,
+          key: true,
+        },
+        name: {
+          type: Types.STRING,
+          index: true,
+        },
+        age: Types.NUMBER,
+      });
+    } catch (e) {
+      console.log(e.message);
+      throw e;
+    }
   }).toThrow();
 
-  // #4. to be not thrown exception
+  // #4. trying to register same model name (but, diffrent target version)
   expect(() => {
-    // trying to register same model name (but, diffrent target version)
     box.model(2)('user', {
+      id: {
+        type: Types.NUMBER,
+        key: true,
+      },
+      name: {
+        type: Types.STRING,
+        index: true,
+      },
+      age: {
+        type: Types.NUMBER,
+      },
+    });
+  }).not.toThrow();
+});
+
+test('Try change keyPath/index', () => {
+  // Change keyPath
+  expect(() => {
+    try {
+      box.model(3)('user', {
+        // In version 1, 2 has key
+        // Now remove key option
+        id: Types.NUMBER,
+        name: {
+          type: Types.STRING,
+          index: true,
+        },
+        age: Types.NUMBER,
+      });
+    } catch (e) {
+      console.log(e.message);
+      throw e;
+    }
+  }).toThrow();
+
+  // Change index
+  expect(() => {
+    box.model(3)('user', {
+      id: {
+        type: Types.NUMBER,
+        key: true,
+      },
+      // In version 1, 2 has index
+      // Now remove index option
       name: Types.STRING,
       age: Types.NUMBER,
     });
-  }).not.toThrow();
+  });
 });
