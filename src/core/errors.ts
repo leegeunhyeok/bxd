db08203a0a70d3f799e3376096656442b0a9ac07
leegeunhyeok <dev.ghlee@gmail.com>
2021-03-31 16:27:43 +0900

    feat: update BoxDBError

--- src/core/errors.ts
+++ src/core/errors.ts
@@ -1,6 +1,20 @@
-export class BoxDBError extends Error {
-  constructor(msg: string) {
-    super(msg);
-    this.name = 'BoxDBError';
-  }
+interface BoxDBError {
+  name: string;
+  message: string;
+  stack?: string;
 }
+
+interface BoxDBErrorConstructor {
+  new (message?: string): BoxDBError;
+  (message?: string): BoxDBError;
+  readonly prototype: Error;
+}
+
+function BxdError(message?: string): void {
+  this.name = 'BoxDBError';
+  this.message = message;
+}
+
+BxdError.prototype = Error.prototype;
+
+export const BoxDBError = BxdError as BoxDBErrorConstructor;
